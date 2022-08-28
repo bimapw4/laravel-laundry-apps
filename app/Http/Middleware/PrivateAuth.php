359fb502ddb051c423587d6bc7d46ebc9d5faad8
bimapw4 <bimapw4@gmail.com>
2022-08-28 20:10:03 +0700

    bug fix header

--- app/Http/Middleware/PrivateAuth.php
+++ app/Http/Middleware/PrivateAuth.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Exceptions\UnauthorizedException;
 use App\Services\Auth\AuthManager;
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 
 class PrivateAuth
@@ -19,20 +20,21 @@ class PrivateAuth
     public function handle(Request $request, Closure $next)
     {
        try {
-            $jwt = $request->header("X-Token");
+            $jwt = $request->header("token");
             $user = $this->ValidateToken($jwt);
 
             $request->request->add(["userdata" => $user["data"]]);
             return $next($request);
             // return $user;
-       } catch (\UnauthorizedException $th) {
+       } catch (\Exception $e) {
+            throw new UnauthorizedException($e->getMessage(), 422);
        }
     }
 
     public function ValidateToken($jwt)
     {
         if (!$jwt) {
-            throw new UnauthorizedException("x-token cannot be empty!", 422);
+            throw new Exception("token cannot be empty!");
         }
 
         return (new AuthManager)->DecodeJWT($jwt);
