cc24d6ae371af4321209f72686e3676921984da8
bimapw4 <bimapw4@gmail.com>
2022-08-31 11:03:01 +0700

    add new model layanan

---
+++ app/Models/Layanan.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Layanan extends Model
+{
+    
+    use SoftDeletes;
+
+    protected $table = "barang";
+    protected $primaryKey = "id_layanan";
+
+    protected $fillable = [
+        "nama_layanan",
+        "biaya"
+    ];
+
+    protected $dates = ['deleted_at'];
+    protected $hidden = ['deleted_at','created_at','updated_at'];
+}
